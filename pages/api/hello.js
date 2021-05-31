5d72d74f895b6090fadfd67936001032cebbf175
yoko <hydra666kyo@gmail.com>
2021-05-31 15:55:39 +0900

    hoge

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,10 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-<<<<<<< HEAD
-  res.status(200).json({ name: 'John Doe' })
-=======
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
->>>>>>> deploy/master
-}
+  res.status(200).json({ name: "John Doe" });
+};
