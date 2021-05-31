fc0b89e20246d334ac8c48607746d815852398ee
yoko <hydra666kyo@gmail.com>
2021-05-31 22:00:24 +0900

    hoge

--- components/atoms/index.js
+++ components/atoms/index.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 import Text from "./Text";
 import DetailButton from "./DetailButton";
 import AnimationButton from "./AnimationButton.jsx";
 
 export { Text, DetailButton, AnimationButton };
-=======
-import Text from "./Text";
-import DetailButton from "./DetailButton";
-
-export { Text, DetailButton };
->>>>>>> deploy/main
